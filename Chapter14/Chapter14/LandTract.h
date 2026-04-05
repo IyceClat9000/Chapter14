78ef173498b08ed07bebe45b3c4d82e027238ff2
DebuggingMyDegree <sr59283@student.sac.edu>
2026-04-05 09:36:06 -0700

    Create LandTract.h

---
+++ Chapter14/Chapter14/LandTract.h
@@ -0,0 +1,43 @@
+#pragma once
+#ifndef LANDTRACT_H
+#define LANDTRACT_H
+
+#include "FeetInches.h"
+
+class LandTract
+{
+private:
+	FeetInches length;
+	FeetInches width;
+
+public:
+	// Constructors
+	// Precondition: None
+	// Postcondition: length and width are set to 0 feet and 0 inches
+	LandTract();
+
+	// Constructors
+	// Precondition: l and w should be defined objects with positive feet and inches, inches should be in the range 1-12
+	// Postcondition: length and width are set to the given parameters. Length = L, Width = W
+	LandTract(FeetInches l, FeetInches w);
+
+	// Mutators
+	void setLength(FeetInches l);
+	void setWidth(FeetInches w);
+
+	// Accessors
+	FeetInches getLength() const;
+	FeetInches getWidth() const;
+
+	// getArea Function
+	// Precondition: length and width are valid
+	// Postcondition: returns the area of the land tract as a FeetInches object
+	FeetInches getArea() const;
+
+	// Operator==
+    // Precondition: Both objects have valid dimensions
+    // Postcondition: Returns true if the area are equal
+	bool operator ==(const LandTract& right) const;
+};
+
+#endif 
